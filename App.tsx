bfce3bc5516ce7bf834c657cbd402ae49a0a0a28
Daniel Williams <dannyhyunsoowilliams@gmail.com>
2024-05-10 17:32:52 +0100

    reproduction

--- App.tsx
+++ App.tsx
@@ -1,10 +1,19 @@
 import { ScreenContent } from 'components/ScreenContent';
 import { StatusBar } from 'expo-status-bar';
+import { MotiView } from 'moti';
 
 export default function App() {
   return (
     <>
       <ScreenContent title="Home" path="App.tsx" />
+      <MotiView
+        from={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{
+          opacity: 0,
+        }}
+        exitTransition={{ duration: 300 }}
+      />
       <StatusBar style="auto" />
     </>
   );
